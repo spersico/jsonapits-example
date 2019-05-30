4f15e8688492351021b0b695e8f6a1eac48b185a
Santiago Persico <santiagopersico@gmail.com>
2019-05-30 18:31:12 -0300

    menos!

--- app/callbacks/login.ts
+++
@@ -1,5 +0,0 @@
-import { Operation, ResourceAttributes } from "@ebryn/jsonapi-ts";
-
-export default async (op: Operation, user: ResourceAttributes) => {
-  return op.data.attributes.email === user.email && op.data.attributes.password === user.password;
-}
